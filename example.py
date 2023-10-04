ac9aca5fb2ae8c6f66018d0fb71fe8d205e9ac76
Jaro Camphuijsen <jjecamphuijsen@gmail.com>
2023-10-04 08:27:15 +0200

    Implement add and subtract function with test for add

---
+++ example.py
@@ -0,0 +1,9 @@
+def add(a, b):
+    return a + b
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add('space', 'ship') == 'spaceship'
+
+def subtract(a, b):
+    return a + b  # do not change this line until prompted to do so.
